489a41213e153bc20a688c91a7c902522c3af4e7
23CABSHIYAMR <shiyamramesh01@gmail.com>
2025-10-02 11:12:43 +0530

    first commit with all configs

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    Hiii
+    <p className='w-fit p-3 flex align-middle justify-center border-2 border-amber-950 text-blue'>
+      Check css
+    </p>
+    </>
+  )
+}
+
+export default App
